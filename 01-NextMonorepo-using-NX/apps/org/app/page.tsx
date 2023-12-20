17cb6ce72be8ce8a58d6687d31b9347c88336ec5
Being-Murshid <murshid71704@gmail.com>
2023-12-19 23:44:28 -0800

    app : create next monorepo using nx

---
+++ 01-NextMonorepo-using-NX/apps/org/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return <div>hello world </div>;
+}
