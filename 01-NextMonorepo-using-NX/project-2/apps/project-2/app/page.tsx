98ae9cd201cde43f4650514c09cb2fda03d042ca
Being-Murshid <murshid71704@gmail.com>
2024-01-19 02:51:52 -0800

    create nx application

---
+++ 01-NextMonorepo-using-NX/project-2/apps/project-2/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from './page.module.css';
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+   <div>
+    hello world 
+   </div>
+  );
+}
