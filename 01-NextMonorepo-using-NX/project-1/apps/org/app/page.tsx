940fe95e08a810dc7162051f283ebf1d684568f3
Being-Murshid <murshid71704@gmail.com>
2024-03-11 03:40:36 -0700

    create a next app and also integrate nx

--- 01-NextMonorepo-using-NX/project-1/apps/org/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default async function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
-  return <div>hello world </div>;
-}
