aa7e35ee2736746d84db1c1b1c8836c8e4359d34
Being-Murshid <murshid71704@gmail.com>
2024-03-12 00:09:50 -0700

    learn nx

--- 01-NextMonorepo-using-NX/myapp01/src/app/page.tsx
+++ 01-NextMonorepo-using-NX/myapp01/src/app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+            saqi
             By{' '}
             <Image
               src="/vercel.svg"
