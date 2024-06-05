e431be3f6219bcf49e20f06a612c190cfdbb64f6
m-saqlain024 <m.saqlain0241@gmail.com>
2024-06-04 23:15:31 -0700

    t03a01 create next js app

--- 03-Trpc/my-app/src/app/_trpc/serverClient.ts
+++
@@ -1,6 +0,0 @@
-import { appRouter } from "@/trpc";
-import { httpBatchLink } from "@trpc/client";
-
-export const serverClient = appRouter.createCaller({
-  links: [httpBatchLink({ url: "http://localhost:3000/api/trpc" })],
-});
--- 03-Trpc/myapp-05/src/pages/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-   <>
-   <h1> this is a new app for pratice trpc </h1>
-   </>
-  )
-}
