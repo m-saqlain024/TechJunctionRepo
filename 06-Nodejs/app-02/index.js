1adf6300099d6d9d44a4e7568f0b083535c6c117
m-saqlain024 <m.saqlain0241@gmail.com>
2024-06-10 00:22:44 -0700

    t03a02 write a own modue

--- 06-Nodejs/app-02/index.js
+++ 06-Nodejs/app-02/index.js
@@ -1,8 +1,13 @@
 var http = require("http");
-
+var url = require("url");
+var dt = require("./createMofule");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello Worlld");
+    // res.write(req.url);
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
+    // res.end(dt.nowdata());
   })
   .listen(8080);
