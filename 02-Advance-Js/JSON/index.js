179d0aff312a801430184efb5d099aee10f5e3a8
m-saqlain024 <m.saqlain0241@gmail.com>
2024-06-20 01:53:52 -0700

    t02fjson learn about json

---
+++ 02-Advance-Js/JSON/index.js
@@ -0,0 +1,30 @@
+let str = '{"name":"John", "age":30, "car":null}';
+
+let data = JSON.parse(str); //json.parse s used to convert json in to js object
+let personName = data.name;
+let personAge = data.age;
+
+// console.log(personName);
+const text = '{"name":"John", "birth":"1986-12-14", "city":"New York"}';
+const obj = JSON.parse(text);
+obj.birth = new Date(obj.birth);
+
+// console.log(obj.birth);
+
+const text1 =
+  '{"name":"John", "age":"function () {return 30;}", "city":"New York"}';
+const obj2 = JSON.parse(text1);
+obj2.age = eval("(" + obj2.age + ")");
+
+// console.log(obj2.age);
+
+const mydata = { name: [1, 2, 3, 4], fnames: ["essa", "abbas"] };
+
+const myjson = JSON.stringify(mydata);
+localStorage.setItem("testitem", myjson);
+
+const text2 = localStorage.getItem("testitem");
+// console.log(text2);
+const text3 = JSON.parse(text2);
+console.log(text3);
+document.getElementById("demo").innerHTML = text3.fnames[0];
