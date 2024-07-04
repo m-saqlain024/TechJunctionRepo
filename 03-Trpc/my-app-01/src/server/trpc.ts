be7f6d6b601427722726a101b901d339123e8e94
m-saqlain024 <m.saqlain0241@gmail.com>
2024-07-04 00:36:24 -0700

    t02 change in file

--- 03-Trpc/my-app-01/src/server/trpc.ts
+++ 03-Trpc/my-app-01/src/server/trpc.ts
@@ -1,7 +1,19 @@
-import { initTRPC } from "@trpc/server";
+/**
+ * This is your entry point to setup the root configuration for tRPC on the server.
+ * - `initTRPC` should only be used once per app.
+ * - We export only the functionality that we use so we can enforce which base procedures should be used
+ *
+ * Learn how to create protected base procedures and other things below:
+ * @link https://trpc.io/docs/v11/router
+ * @link https://trpc.io/docs/v11/procedures
+ */
+import { initTRPC } from '@trpc/server';
 
 const t = initTRPC.create();
 
-export const router = t.router;
-
+/**
+ * Unprotected procedure
+ **/
 export const publicProcedure = t.procedure;
+
+export const router = t.router;
