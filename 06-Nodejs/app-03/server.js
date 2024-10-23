7d45e3a1ec4ba72671c0c88053af605a88ccd607
m-saqlain024 <m.saqlain0241@gmail.com>
2024-10-23 15:16:59 +0500

    install nodemon

--- 06-Nodejs/app-03/server.js
+++ 06-Nodejs/app-03/server.js
@@ -1,8 +1,11 @@
 import http from "http";
 const PORT = 8000;
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/HTML'});
-  res.write("hello world");
+    // res.writeHead(200, {'Content-Type': 'text/HTML'});
+    // res.statusCode = 404;
+    res.writeHead(500 , {'Content-Type': 'application/JSON'})
+  // res.write("hello world");
+  res.write(json.stringify({"Message " : "Server Error"}));
 });
 
 server.listen(PORT, () => {
