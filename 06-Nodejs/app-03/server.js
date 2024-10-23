e2b6330cc071145f932c3b5ab7fad6582fc25d0b
m-saqlain024 <m.saqlain0241@gmail.com>
2024-10-23 16:47:28 +0500

    learn fs , url , path module

--- 06-Nodejs/app-03/server.js
+++ 06-Nodejs/app-03/server.js
@@ -1,13 +1,41 @@
-import http from "http"
-const  PORT = 8000;
+import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 
+const PORT = process.env.PORT;
 
-const server = http.createServer((req, res)=>{
-    res.writeHead(200 , {'Content-Type': 'Text/HTML'})
-    res.end("<h1>hello world </h1>")
-})
+// Get file pathname
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+const server = http.createServer(async(req, res) => {
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error("not found");
+      }
+      const data = await fs.readFile(filePath)
+      res.setHeader('Content-type' , "text/html")
+      res.write(data)
+      res.end()
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("<h1>Server Error</h1>");
+  }
 
-server.listen(PORT ,()=>{
-    console.log(`Server is running : ${PORT}`)
-})
+  // console.log(req.method, "method")
+  // console.log(req.url, "url")
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running : ${PORT}`);
+});
