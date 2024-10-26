0620f4c97688e1f4fae3bf19f61ce09a1ea2c91c
m-saqlain024 <m.saqlain0241@gmail.com>
2024-10-26 18:37:16 +0500

    change file structure

--- 04-Nextjs-and-Tailwindcss/myapp_01/src/containers/HomePage/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AboutMe } from "./AboutSection/AboutSection";
-export { default as HeroSection } from "./HeroSection/HeroSection";
-export { default as PricingSection } from "./PricingSection/PricingSection";
