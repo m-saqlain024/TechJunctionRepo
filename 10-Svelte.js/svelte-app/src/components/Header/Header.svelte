d0a2d2f63ce901efc75ad75d41e7f96b2fd31157
m-saqlain024 <m.saqlain0241@gmail.com>
2024-10-28 19:24:34 +0500

    create a svelte ap

--- 10-Svelte.js/svelte-app/src/components/Header/Header.svelte
+++ 10-Svelte.js/svelte-app/src/components/Header/Header.svelte
@@ -1,47 +1,61 @@
 <script>
-  import './header.css';
-  import Button from '../Button/Button.svelte';
+// @ts-nocheck
 
-  /**
-   * @typedef {Object} Props
-   * @property {{name: string}} [user] The user object
-   * @property {() => void} [onLogin] The login event handler
-   * @property {() => void} [onLogout] The logout event handler
-   * @property {() => void} [onCreateAccount] The account creation event handler
-   */
+	import Button from "../Button/Button.stories.svelte";
 
-  /** @type {Props} */
-  const { user, onLogin, onLogout, onCreateAccount } = $props();
+	let isOpen = false;
+	const navigation = [
+		{ name: 'Home', href: '#' },
+		{ name: 'Team', href: '#' },
+		{ name: 'Projects', href: '#' },
+		{ name: 'Calendar', href: '#' },
+		{ name: 'Reports', href: '#' }
+	];
+
+	function toggleMenu() {
+		isOpen = !isOpen;
+	}
 </script>
 
-<header>
-  <div class="storybook-header">
-    <div>
-      <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-        <g fill="none" fill-rule="evenodd">
-          <path
-            d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
-            fill="#FFF"
-          />
-          <path
-            d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
-            fill="#555AB9"
-          />
-          <path d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z" fill="#91BAF8" />
-        </g>
-      </svg>
-      <h1>Acme</h1>
-    </div>
-    <div>
-      {#if user}
-        <span class="welcome">
-          Welcome, <b>{user.name}</b>!
-        </span>
-        <Button size="small" onClick={onLogout} label="Log out" />
-      {:else}
-        <Button size="small" onClick={onLogin} label="Log in" />
-        <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-      {/if}
-    </div>
-  </div>
+<header class="w-full">
+	<div class="mx-auto max-w-[1400px] px-4 sm:px-6 lg:px-8">
+		<nav class="flex items-center justify-between gap-4 py-4">
+			<!-- Logo or Brand Name -->
+			<div class="text-xl  font-bold">Brand</div>
+
+			<!-- Hamburger Button for Mobile -->
+			<!-- svelte-ignore a11y_consider_explicit_label -->
+			<button class=" md:hidden" on:click={toggleMenu}>
+				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+				</svg>
+			</button>
+
+			<!-- Desktop Navigation Links -->
+			<div class="hidden md:flex gap-4">
+				{#each navigation as item}
+					<a href={item.href} class="px-3 py-2 text-lg font-normal hover:bg-gray-300  rounded">{item.name}</a>
+				{/each}
+			</div>
+
+			<!-- Desktop Buttons -->
+			<div class="hidden md:flex gap-3 items-center">
+				<button class="text-white px-4 py-2 bg-blue-700 rounded hover:bg-blue-600">Login</button>
+				<button class="text-white px-4 py-2 bg-blue-700 rounded hover:bg-blue-600">Sign up</button>
+			</div>
+		</nav>
+
+		<!-- Mobile Navigation Links -->
+		{#if isOpen}
+			<div class="md:hidden bg-blue-600">
+				{#each navigation as item}
+					<a href={item.href} class="block px-4 py-2 text-white hover:bg-blue-700">{item.name}</a>
+				{/each}
+				<div class="flex flex-col gap-3 items-start px-4 py-2">
+					<button class="text-white px-4 py-2 bg-blue-700 rounded hover:bg-blue-600">Login</button>
+					<button class="text-white px-4 py-2 bg-blue-700 rounded hover:bg-blue-600">Sign up</button>
+				</div>
+			</div>
+		{/if}
+	</div>
 </header>
