f477ee201cfa937192911102a52822a4729c683a
m-saqlain024 <m.saqlain0241@gmail.com>
2024-10-30 14:31:42 +0500

    create some route

--- 06-Express.js/server/server.js
+++ 06-Express.js/server/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const path = require('path')
+
+const PORT = process.env.PORT  || 3000
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.end("Hello world ");
+  res.sendFile(path.join(__dirname , "public" , "index.html"))
 });
 
-app.listen(8000, () => console.log("server is running"));
+app.get('/about' , (req ,res)=>{
+    res.sendFile(path.join(__dirname , "public" , "about.html"))
+
+})
+
+app.listen(PORT, () => console.log(`Server is running pn : ${PORT}`));
