462a8a2e7f7965a16fca7048ad42f47c57a753da
m-saqlain024 <m.saqlain0241@gmail.com>
2024-10-30 14:40:14 +0500

    create a static pages

--- 06-Express.js/server/server.js
+++ 06-Express.js/server/server.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const path = require('path')
+const path = require("path");
 
-const PORT = process.env.PORT  || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname , "public" , "index.html"))
-});
+// setup static floder
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/about' , (req ,res)=>{
-    res.sendFile(path.join(__dirname , "public" , "about.html"))
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname , "public" , "index.html"))
+// });
 
-})
+// app.get('/about' , (req ,res)=>{
+//     res.sendFile(path.join(__dirname , "public" , "about.html"))
+
+// })
 
 app.listen(PORT, () => console.log(`Server is running pn : ${PORT}`));
